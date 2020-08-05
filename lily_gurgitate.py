b8b00d1485ab3e4faf2323097a6106f87744f58b
noah husband <noah@noahs-MacBook-Pro.local>
2020-08-04 18:12:34 -0700

    prints less

--- lily_gurgitate.py
+++ lily_gurgitate.py
@@ -1,20 +1,17 @@
-#!/usr/bin/env python3
-
 import argparse
 import motiflib
 import statistics
 
-
 parser = argparse.ArgumentParser(
-	description='Do motif instances look like motifs?')
+    description='Do motif instances look like motifs?')
 parser.add_argument('--file', required=True, type=str,
-	metavar='<str>', help='motif file in JASPAR format')
+                    metavar='<str>', help='motif file in JASPAR format')
 parser.add_argument('--seqs', required=False, type=int, default=5,
-	metavar='<int>', help='sequences to generate [%(default)i]')
+                    metavar='<int>', help='sequences to generate [%(default)i]')
 parser.add_argument('--iterations', required=False, type=int, default=5,
-	metavar='<int>', help='sequences to generate [%(default)i]')
+                    metavar='<int>', help='sequences to generate [%(default)i]')
 parser.add_argument('--end', required=False, type=int, default=100,
-	metavar='<int>', help='sequences to generate [%(default)i]')
+                    metavar='<int>', help='sequences to generate [%(default)i]')
 arg = parser.parse_args()
 
 n = arg.iterations
@@ -24,27 +21,21 @@ distances = []
 motif = motiflib.read_JASPAR(arg.file)
 print()
 for i in range(end):
-	
-	for j in range(n): 
-		sites = []
-		for k in range(arg.seqs):
-			site = motiflib.generate_site(motif)
-			sites.append(site)
-		pwm = motiflib.new_pwm(sites)
-		distance = motiflib.compare_motifs(motif, pwm)
-		distances.append(distance)
-		print(distances)
-	print(distances)
-	avg_distance = statistics.mean(distances)
-	print(i,avg_distance)
-	
+
+    for j in range(n):
+        sites = []
+        for k in range(arg.seqs):
+            site = motiflib.generate_site(motif)
+            sites.append(site)
+        pwm = motiflib.new_pwm(sites)
+        distance = motiflib.compare_motifs(motif, pwm)
+        distances.append(distance)
+        #print(distances)
+    #print(distances)
+    avg_distance = statistics.mean(distances)
+    print(i, avg_distance)
+
 # if you collected the sites back into a motif, would it look like the motif?
 # how would you measure the similarity?
 # you might want to run this many times to look at the variance
-#print(sites)
-
-	
-
-
-
-
+# print(sites)
